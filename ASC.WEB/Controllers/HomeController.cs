825aca0a78d5c83aa331062307d47565f11954b6
Le-Van-Nam10 <namok09052004@gmail.com>
2025-03-12 20:24:41 +0700

    Up code lên TH_TKPM_Lab3

--- ASC.WEB/Controllers/HomeController.cs
+++ ASC.WEB/Controllers/HomeController.cs
@@ -1,26 +1,54 @@
-using ASC.WEB.Configuration;
-using ASC.WEB.Models;
+﻿using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using System.Diagnostics;
+
+using ASC.WEB.Services;
+using Microsoft.Identity.Client;
+using ASC.Utilities;
+using ASC.WEB.Configuration;
+using ASC.WEB.Models;
 
 namespace ASC.WEB.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
         private IOptions<ApplicationSettings> _settings;
-
-        public HomeController(ILogger<HomeController> logger , IOptions<ApplicationSettings> settings)
+        public HomeController(
+            //ILogger<HomeController> logger, 
+            IOptions<ApplicationSettings> settings)
         {
-            _logger = logger;
+            //_logger = logger;
             _settings = settings;
-
         }
 
+        //public HomeController(IOptions<ApplicationSettings> @object)
+        //{
+        //}
+
+        //public IActionResult Index([FromServices] IEmailSender emailSender)
+        //{
+        //    var emailService = this.HttpContext.RequestServices.GetService(typeof(IEmailSender)) as IEmailSender;
+        //    ViewBag.Title = _settings.Value.ApplicationTitle;
+        //    return View();
+        //}
         public IActionResult Index()
         {
-            ViewBag.Title = _settings.Value.ApplicationTitle;
+            // Thiết lập Session
+            HttpContext.Session.SetSession("Test", _settings.Value);
+
+            // Lấy Session
+            var settings = HttpContext.Session.GetSession<ApplicationSettings>("Test");
+
+            // Sử dụng IOptions
+            ViewBag.Title = settings.ApplicationTitle;
+
+            // Trường hợp kiểm thử thất bại (đã comment)
+            // ViewData.Model = "Test";
+            // throw new Exception("Đăng nhập thất bại!!!");
+
             return View();
         }
 
@@ -38,5 +66,14 @@ namespace ASC.WEB.Controllers
         {
             return View();
         }
+
+
+        //public IActionResult Index()
+        //{
+        //    //Test fail test case
+        //    ViewData.Model = "Test";
+        //    throw new Exception("Login Fail!!!");
+        //    return View();
+        //}
     }
 }
